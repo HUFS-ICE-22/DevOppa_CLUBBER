ad7162d4a5de463e036342d7c3a4c3de670fe356
benny <benny-kim@naver.com>
2023-06-24 06:29:46 +0900

    동아리 View 출력부 다듬음

--- app/src/main/java/com/example/hackathon/BoardActivity.java
+++ app/src/main/java/com/example/hackathon/BoardActivity.java
@@ -29,6 +29,10 @@ public class BoardActivity extends AppCompatActivity {
     List<Board> mDatas = new ArrayList<>(); // 샘플 데이터 추가
     BoardAdapter mAdapter = new BoardAdapter(mDatas);;
 
+    //사용자가 포함된 그룹 정보 접근
+    DatabaseReference userRef = database.child("clubs");
+    static String settingKey;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +40,16 @@ public class BoardActivity extends AppCompatActivity {
 
         database = FirebaseDatabase.getInstance().getReference();
         firebaseAuth = FirebaseAuth.getInstance();
+
+        Button button10 = findViewById(R.id.button10);
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RecyclerView mPostRecyclerView = findViewById(R.id.recyclerView);
+                getGroups();
+                mPostRecyclerView.setAdapter(mAdapter);
+            }
+        });
 
         RecyclerView mPostRecyclerView = findViewById(R.id.recyclerView);
 //        List<Board> mDatas = new ArrayList<>(); // 샘플 데이터 추가
@@ -58,8 +72,6 @@ public class BoardActivity extends AppCompatActivity {
         //현재 사용자의 UID를 가져옴
 //        String userId = firebaseAuth.getCurrentUser().getUid();
 
-        //사용자가 포함된 그룹 정보 접근
-        DatabaseReference userRef = database.child("clubs");
         userRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -72,11 +84,30 @@ public class BoardActivity extends AppCompatActivity {
                     groupRef.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                            String content = "";
+                            //그룹명 가져오기
+
+                            if (dataSnapshot.child("모집중").getValue(Boolean.class) == true) {
+                                content += "모집중";
+                            } else {
+                                content += "모집기간 아님";
+                            }
+
+                            if (dataSnapshot.child("categories").child("교내").getValue(Boolean.class) == true) {
+                                content += " | 교내동아리";
+                            } else {
+                                content += " | 연합동아리";
+                            }
+
+                            content += " | ";
+                            content += dataSnapshot.child("categories").child("category").getValue(String.class);
+
                             //그룹명 가져오기
                             String groupContents = dataSnapshot.child("onelistsummary").getValue(String.class);
                             Log.d("MainActivity", "onCreate - onClick : " + groupContents);
                             //그룹명을 리스트에 추가
-                            mDatas.add(new Board(groupName, groupContents,"time",20,10));
+                            mDatas.add(new Board(groupName, content,"time",20,10));
                             Log.d("MainActivity1", "onCreate - onClick : " + mDatas);
 
                             mAdapter.notifyDataSetChanged();

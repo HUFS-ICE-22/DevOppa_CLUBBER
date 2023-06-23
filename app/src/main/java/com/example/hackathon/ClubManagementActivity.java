656a1f52105ab648831b591c5dba756046f9ad3c
multiful <rlaehdrb2485@naver.com>
2023-06-24 00:54:25 +0900

    동아리 추가 화면 생성

--- app/src/main/java/com/example/hackathon/ClubManagementActivity.java
+++ app/src/main/java/com/example/hackathon/ClubManagementActivity.java
@@ -1,10 +1,16 @@
 package com.example.hackathon;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,7 +18,11 @@ public class ClubManagementActivity extends AppCompatActivity {
 
     private LinearLayout layoutClubOfficials;
     private Button buttonAddOfficial;
+    private Button buttonChooseImage;
     private Button buttonSave;
+    private Button buttonRemoveOfficial;
+    private ImageView imageViewMainImage;
+    private int REQUEST_IMAGE_GALLERY;
 
     private int officialCount = 0;
 
@@ -23,14 +33,39 @@ public class ClubManagementActivity extends AppCompatActivity {
 
         layoutClubOfficials = findViewById(R.id.layoutClubOfficials);
         buttonAddOfficial = findViewById(R.id.buttonAddOfficial);
+        buttonChooseImage = findViewById(R.id.buttonChooseImage);
+        buttonRemoveOfficial = findViewById(R.id.buttonRemoveOfficial);
+
         buttonSave = findViewById(R.id.buttonSave);
+
+        // 버튼 클릭 이벤트 핸들러
+        buttonChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 갤러리에서 사진을 선택하기 위한 인텐트 생성
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+
+                // 갤러리 앱 호출 및 결과 처리를 위한 startActivityForResult 사용
+                startActivityForResult(intent, REQUEST_IMAGE_GALLERY);
+            }
+        });
 
         buttonAddOfficial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 addOfficialInputFields();
+                buttonRemoveOfficial.setVisibility(View.VISIBLE); // 수정된 부분
             }
         });
+
+        buttonRemoveOfficial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeOfficialInputFields();
+            }
+        });
+
 
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,12 +83,16 @@ public class ClubManagementActivity extends AppCompatActivity {
 
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
+                LinearLayout.LayoutParams.MATCH_PARENT
         );
 
         LinearLayout layoutOfficial = new LinearLayout(this);
         layoutOfficial.setOrientation(LinearLayout.VERTICAL);
         layoutOfficial.setLayoutParams(layoutParams);
+
+        TextView titleText = new TextView(this);
+        titleText.setText("임원 정보 추가");
+        layoutOfficial.addView(titleText);
 
         EditText editTextName = new EditText(this);
         editTextName.setHint("이름");
@@ -85,6 +124,33 @@ public class ClubManagementActivity extends AppCompatActivity {
             String major = editTextMajor.getText().toString();
 
             // 동아리 임원 정보를 저장하는 로직을 구현
+        }
+    }
+    private void removeOfficialInputFields() {
+        if (officialCount > 0) {
+            layoutClubOfficials.removeViewAt(officialCount - 1);
+            officialCount--;
+
+            // 모든 임원 정보 입력 필드를 삭제했을 경우, 삭제 버튼을 다시 숨깁니다
+            if (officialCount == 0) {
+                buttonRemoveOfficial.setVisibility(View.GONE);
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_GALLERY && resultCode == RESULT_OK) {
+            if (data != null) {
+                Uri selectedImageUri = data.getData();
+
+                // 선택한 이미지를 가져와서 사용할 수 있습니다.
+                // 예를 들어, ImageView에 표시하거나 업로드 등의 작업을 수행할 수 있습니다.
+                imageViewMainImage.setImageURI(selectedImageUri);
+            }
         }
     }
 }

8cdad5b014e9d315cbc13494eb3ee735809bc31d
woorym <woorym2@naver.com>
2023-06-23 21:54:36 +0900

    로그인 회원가입

--- app/src/main/java/com/example/hackathon/BoardActivity.java
+++ app/src/main/java/com/example/hackathon/BoardActivity.java
@@ -2,7 +2,10 @@ package com.example.hackathon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BoardActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class BoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+
+        Button go_sign = findViewById(R.id.button_go_sign);
+
+        go_sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Sign_in.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
